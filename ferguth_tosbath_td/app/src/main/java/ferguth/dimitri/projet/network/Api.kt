147ec1243c6ab02d4349b7e0f6f1ed9e98d51c07
DimSmoke <dimitri.ferguth@devinci.fr>
2020-01-04 23:52:52 +0100

    Projet+vidéo

---
+++ ferguth_tosbath_td/app/src/main/java/ferguth/dimitri/projet/network/Api.kt
@@ -0,0 +1,32 @@
+package ferguth.dimitri.projet.network
+
+import ferguth.dimitri.projet.TasksService
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object Api {
+    private const val BASE_URL = "https://android-tasks-api.herokuapp.com/api/"
+    private const val TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxNzMsImV4cCI6MTYwOTc5NjU5M30.FqNxcQchoag9qFgp0yz6yjpXpQ39dmy9DqJBV123u7k"
+    private val moshi = Moshi.Builder().build()
+
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $TOKEN")
+                    .build()
+                chain.proceed(newRequest)
+            }
+            .build()
+    }
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
+    val userService: UserService by lazy { retrofit.create(UserService::class.java) }
+    val tasksService: TasksService by lazy { retrofit.create(TasksService::class.java) }
+}
